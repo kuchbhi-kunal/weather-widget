88e7f372ca99a581e1711bb981bf8664597cae5d
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2024-04-02 00:16:50 +0530

    city added

--- script.js
+++ script.js
@@ -18,18 +18,60 @@ function displayWeather(data) {
     var weatherInfoDiv = document.getElementById("weatherInfo");
     var highestTemp = Math.round(data.main.temp_max);
     var lowestTemp = Math.round(data.main.temp_min);
+    var city = data.name;
+
+    // Set image source URL based on the city
+    var imageUrl;
+    switch(city) {
+        case 'Mumbai':
+            imageUrl = 'mumbai.png';
+            break;
+        case 'Bengaluru':
+            imageUrl = 'bangalore.png';
+            break;
+        case 'Delhi':
+            imageUrl = 'delhi.png';
+            break;
+        default:
+            imageUrl = 'default.png';
+            break;
+    }
+
+      // Set background image URL based on the city
+      var bgGifUrl;
+      switch(city) {
+          case 'Mumbai':
+              bgGifUrl = 'perfect.gif';
+              break;
+          case 'Bengaluru':
+              bgGifUrl = 'perfect1.gif';
+              break;
+          case 'Delhi':
+              bgGifUrl = 'perfect2.gif';
+              break;
+          default:
+              bgGifUrl = 'default.gif';
+              break;
+      }
+  
+      weatherInfoDiv.style.background = `url('${bgGifUrl}') no-repeat center center`;
+
     weatherInfoDiv.innerHTML = `
         <div>
             <h2 class="cityname">${data.name}, ${data.sys.country}</h2>
             <p class="temp">${Math.round(data.main.temp)}</p>
         </div>
-        <div class="gateway"><img src="https://github.com/kuchbhi-kunal/weather-widget/blob/main/gateway.png?raw=true" loading="lazy"/></div>
+        <div class="gateway"><img src="${imageUrl}" loading="lazy"/></div>
         <div class="tempfooter">
             <p>H : ${highestTemp}°C</p><span>
             <p>L : ${lowestTemp}°C</p>
         </div>
     `;
-
-    
 }
 
+
+function changeCity() {
+    var citySelector = document.getElementById("citySelector");
+    var selectedCity = citySelector.value;
+    fetchWeatherData(selectedCity);
+}
